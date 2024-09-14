31e306e46c1e0fbd78381a72d31f5caeae6467a2
hgcvm <hgcvm@example.com>
2024-09-14 23:24:28 +0200

    local update of notes

--- BEnotes_without_hashtag.sh
+++ BEnotes_without_hashtag.sh
@@ -2,19 +2,13 @@
 
 #Lijst met alle open notes in BE aanmaken
 wget -O scraped.txt "https://resultmaps.neis-one.org/osm-notes-country-custom?c=Belgium&query=open"
-LISTOFNOTES=`html2text scraped.txt | grep -E "\[[0-9]" | grep -o -P '(?<=\[).*(?=\])'`
-html2text -help
-html2text --mark-code scraped.txt
-
+LISTOFNOTES=`html2text --mark-code scraped.txt | grep -E "\[[0-9]" | grep -o -P '(?<=\[).*(?=\])'`
 echo "$LISTOFNOTES"
 
 #Via API alle notes downloaden
-#cat listofnotes.txt
-#echo " "
-#for i in `cat listofnotes.txt` ; do
-#echo line "$i"
-#wget -O notes/$i https://api.openstreetmap.org/api/0.6/notes/$i
-#done;
+#while IFS= read -r line; do
+#wget -O notes/$line https://api.openstreetmap.org/api/0.6/notes/$line
+#done <<< "$LISTOFNOTES"
 
 #In deze notes inverted zoeken naar ..., en hiervan een HTML maken
 echo "creating HTML"
