d791a6f2282a8413336767c6d078c1c37796136f
hgcvm <hgcvm@example.com>
2024-09-15 10:25:51 +0200

    local update of notes

--- BEnotes_without_hashtag.sh
+++ BEnotes_without_hashtag.sh
@@ -5,15 +5,17 @@ wget -O scraped.txt "https://resultmaps.neis-one.org/osm-notes-country-custom?c=
 LISTOFNOTES=`html2text --mark-code scraped.txt | grep -E "\[[0-9]" | grep -o -P '(?<=\[).*(?=\])'`
 
 #Via API alle notes downloaden
+rm notes/*
 while IFS= read -r line; do
 wget -O notes/$line https://api.openstreetmap.org/api/0.6/notes/$line
+sleep 1
 done <<< "$LISTOFNOTES"
 
 #In deze notes inverted zoeken naar ..., en hiervan een HTML maken
 echo "creating HTML"
 rm index.html
 cd notes/
-FILTEREDLIST=`grep -L -i surveyneeded * | awk -F ":" '{ print $1}' | sort | uniq`
+FILTEREDLIST=`grep -L -i -E "(#SurveyNeeded|#FollowUp)" * | awk -F ":" '{ print $1}' | sort | uniq`
 COUNT=`echo "$FILTEREDLIST" | wc -l`
 while IFS= read -r line ; do 
 echo '<a href="https://www.openstreetmap.org/note/'$line'">'$line'</a><br>' >> ../index.html
@@ -21,6 +23,6 @@ done <<< "$FILTEREDLIST"
 cd ..
 
 DATE=`date`
-sed -i "1s/^/<!DOCTYPE html><html><body>Last updated on $DATE<br>Total count is $COUNT<br>\n/" index.html
+sed -i "1s/^/<!DOCTYPE html><html><body>Last updated on $DATE<br>Total count is $COUNT<br>Currently filtered: #SurveyNeeded #FollowUp<br>\n/" index.html
 sed -i '$ s_$_ </body></html>_' index.html
-
+rm scraped.txt
